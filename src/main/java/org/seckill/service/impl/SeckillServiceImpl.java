d3db083d6a7957f514f5a2b91aa571420af8167f
xiaoelevener <389855286@qq.com>
2017-11-21 23:19:44 +0800

    4th day

--- src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
+++ src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
@@ -12,6 +12,9 @@ import org.seckill.exception.RepeatKillException;
 import org.seckill.exception.SeckillCloseException;
 import org.seckill.exception.SeckillException;
 import org.seckill.service.SeckillService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.DigestUtils;
 
 import java.util.Date;
@@ -22,10 +25,13 @@ import java.util.List;
  * @create 2017-11-20 22:44
  */
 @Slf4j
+@Service
 public class SeckillServiceImpl implements SeckillService {
 
+    @Autowired
     private SeckillDao seckillDao;
 
+    @Autowired
     private SuccessKilledDao successKilledDao;
 
     private final String SLAT = "sf5456gsd5-s1b2w.efe6";
@@ -64,8 +70,15 @@ public class SeckillServiceImpl implements SeckillService {
 
     }
 
+    @Transactional
+    /**
+     * 使用注解控制事务的优点：
+     * 1：开发团队达成一致约定，明确标注事务方法的编程风格
+     * 2：保证事务方法的执行时间尽可能短，不要穿插其他网络操作
+     * 3：不是所有的方法都需要事务
+     */
     public SeckillExecution excuteSeckill(long seckillId, long userPhone, String md5) throws SeckillException, RepeatKillException, SeckillCloseException {
-        if (md5 == null || md5.equals(getMD5(seckillId))) {
+        if (md5 == null || !md5.equals(getMD5(seckillId))) {
             throw new SeckillException("seckill data rewrite");
         }
         try {
---
+++ src/test/java/org/seckill/service/impl/SeckillServiceImplTest.java
@@ -0,0 +1,68 @@
+package org.seckill.service.impl;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.seckill.dao.SeckillDao;
+import org.seckill.dto.Exposer;
+import org.seckill.dto.SeckillExecution;
+import org.seckill.entity.Seckill;
+import org.seckill.exception.RepeatKillException;
+import org.seckill.exception.SeckillCloseException;
+import org.seckill.service.SeckillService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/spring-dao.xml","classpath:spring/spring-service.xml"})
+@Slf4j
+public class SeckillServiceImplTest {
+
+    @Autowired
+    private SeckillService seckillService;
+
+    @Test
+    public void getSeckillList() throws Exception {
+        List<Seckill> result=seckillService.getSeckillList();
+        log.info("【getSeckillList】list={}",result);
+    }
+
+    @Test
+    public void getById() throws Exception {
+        long id=1000L;
+        Seckill result = seckillService.getById(id);
+        log.info("【result】={}", result);
+    }
+
+    @Test
+    public void seckillLogic() throws Exception {
+        long id = 1000L;
+        Exposer exposer = seckillService.exportSeckillUrl(id);
+        if(exposer.isExposed()){
+            log.info("result={}", exposer);
+            long phone=134524456L;
+            String md5=exposer.getMd5();
+            try {
+                SeckillExecution execution = seckillService.excuteSeckill(id, phone, md5);
+                log.info("execution={}", execution);
+            } catch (RepeatKillException e) {
+                log.error(e.getMessage());
+            } catch (SeckillCloseException e) {
+                log.error(e.getMessage());
+            }
+        }else {
+            //秒杀未开启
+            log.warn("expose={}",exposer);
+        }
+
+        /**
+         * result=Exposer(exposed=true, md5=9599d1742f871b9d550507113f4bd695, seckillId=1000, now=0, start=0, end=0)
+         */
+    }
+
+}
